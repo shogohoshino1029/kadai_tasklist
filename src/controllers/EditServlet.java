27423e06e6333cab60f93a4490e865747679aba4
shougo.hoshino <shogo.hoshino.10@gmail.com>
2020-07-26 09:26:42 +0900

    Add EditServlet

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -14,16 +14,16 @@ import models.Tasklist;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ShowServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/show")
-public class ShowServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ShowServlet() {
+    public EditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -39,10 +39,14 @@ public class ShowServlet extends HttpServlet {
 
         em.close();
 
-        // メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
+        // タスクリスト情報とセッションIDをリクエストスコープに登録
         request.setAttribute("tasklist", t);
+        request.setAttribute("_token", request.getSession().getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/show.jsp");
+        // タスクリストIDをセッションスコープに登録
+        request.getSession().setAttribute("tasklist_id", t.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasklists/edit.jsp");
         rd.forward(request, response);
     }
 
